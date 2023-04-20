0cb09a14b2001fb79a8d02d75ba8e79134da23c5
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2023-04-20 06:28:29 +0000

    add module preparing dataset for spacy

---
+++ src/data_preprocessing/prepare_bin.py
@@ -0,0 +1,26 @@
+from spacy.tokens import DocBin
+from spacy.util import filter_spans
+import pandas as pd
+import spacy
+from tqdm.auto import tqdm
+
+def prepare_bin(df: pd.DataFrame, nlp=spacy.blank('ru'), mode='train', option=''):
+    skipped_items = 0
+    doc_bin = DocBin()
+    for idx, item in tqdm(df.iterrows()):
+        text = item['text']
+        doc = nlp.make_doc(text) 
+        if mode != 'test':
+            annotation = item['extracted_part']
+            ents = []
+            span = doc.char_span(annotation['answer_start'][0], annotation['answer_end'][0], label=annotation['label'], alignment_mode="contract")
+            if span is None:
+                    skipped_items += 1
+            else:
+                ents.append(span)
+            filtered_ents = filter_spans(ents)
+            doc.ents = filtered_ents 
+        doc_bin.add(doc)
+    bin_name = option + mode + '_dataset.spacy'
+    doc_bin.to_disk(bin_name)
+    print(f'{skipped_items} items had blank annotation and were skipped')
